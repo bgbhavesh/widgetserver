85501f005089fc96876c3973e9a20588b4dfb7ae
bgbhavesh <bgbhavesh@gmail.com>
2018-08-26 20:12:32 +0530

    push just

--- controllers/common.server.controller.js
+++ controllers/common.server.controller.js
@@ -4,39 +4,82 @@
 //import Notes from '../models/notes.server.model';
 var randomstring = require("randomstring");
 import db from '../api/app';
+var _ = require('lodash/core');
 var ObjectId = require('mongodb').ObjectID;
-export const getAllItems = (req, res) => {
+// reqDemo = {};
+// reqDemo.match;
+// reqDemo.sort;
+// reqDemo.skip;
+// reqDemo.limit;
+// reqDemo.project;
+
+
+// reqDemo.item;
+// reqDemo.itemId;
+// reqDemo.uid
+var createSearchQuery = function (searchText, caseSensitive) {
+    // if(!searchText || searchText==="" || !searchFields || searchFields.length === 0 ) return false;
+    return  { $text: { $search: searchText, $caseSensitive: caseSensitive } }
+}
+var getAggregationArray = function (req) {
     let reqData = req;
-    let reqModel = reqData.model || 'widgets'
-    let queryArray = [
-        function (err, data) {
-            return res.json(data);
-        }
-    ];
-    if (req && reqData && reqData.searchText && reqData.searchText != '') {
-        queryArray.push({ $or: [{ name: reqData.searchText }, { description: reqData.searchText }] })
+    let match = reqData.match || {};
+    let limit = reqData.limit || 0;
+    let skip = reqData.skip || 0;
+    let sort = reqData.sort || {createdBy: -1};
+    let project = reqData.project || 0;
+    let searchText = reqData.searchText || "";
+    let searchFields = reqData.searchFields || false;
+    let aggregateArray = []
+    let searchQuery = createSearchQuery(searchText, searchFields)
+    
+    match = _.extend(match,searchQuery);
+    console.log(match)
+    aggregateArray.push({$match:match});
+    aggregateArray.push({$sort:sort});
+
+    if(skip){
+        aggregateArray.push({$skip:skip});
     }
-    queryArray.unshift({})
+    if(limit){
+        aggregateArray.push({$limit:limit});
+    }
+    if(project){
+        aggregateArray.push({$project:project});
+    }
+    return aggregateArray;
+}
 
-
-
-    console.log(queryArray)
-    db[reqModel].find(queryArray);
+export const getAllItems = (req, res) => {
+    let reqData = req.body.options;
+    let reqModel = reqData.model //|| 'widgets';
+    console.log(reqData);
+    let aggregateArray = getAggregationArray(reqData);
+    
+    db[reqModel].aggregate(aggregateArray,function (err, data) {
+        // console.log(data);
+        return res.json(data);
+    });
+}
+export const getAnItems = (req, res) => {
+    let reqData = req.body.options;
+    console.log(reqData);
+    let reqModel = reqData.model //|| 'widgets';
+    let aggregateArray = getAggregationArray(req);
+    db[reqModel].find({ uid: reqData.uid },function (err, data) {
+        // console.log(data);
+        return res.json(data);
+    });
 }
 export const updateAnItem = (req, res) => {
-    let reqData = req.body;
-    // check(req.model, String)
-    // check(req, Object)
-    // check(req, {
-    //     model: String,
-    //     item: Object
-    // })
+    let reqData = req.body.options;
     if (!reqData || !reqData.model) {
         return res.json({ 'success': false, 'message': 'Model Error' });
     }
-    // console.log('reqData ', reqData)
-    let reqModel = reqData.model || 'widgets'
-    db[reqModel].update({ uid: reqData.item.uid }, reqData.item, function (err, data) {
+    let reqModel = reqData.model //|| 'widgets';
+    reqData.uid = reqData.uid || reqData.item.uid; 
+    reqData.item.updatedAt = new Date();
+    db[reqModel].update({ uid: reqData.uid }, reqData.item, function (err, data) {
         if (data) {
             db[reqModel].find({}, function (err, data) {
                 return res.json(data);
@@ -46,13 +89,15 @@ export const updateAnItem = (req, res) => {
     });
 }
 export const addAnItem = (req, res) => {
-    let reqData = req.body;
-    console.log("reqData", req);
-    if (!reqData || !reqData.model || !reqData.item) {
+    let reqData = req.body.options;
+    if (!(reqData && reqData.model && reqData.item)) {
         return res.json({ 'success': false, 'message': 'Model Error' });
     }
-    reqData.uid = randomstring.generate();
-    let reqModel = reqData.model || 'widgets'
+    reqData.item.uid = randomstring.generate();
+    reqData.item.createdAt = new Date();
+    reqData.item.updateAt = new Date();
+    let reqModel = reqData.model //|| 'widgets'
+    console.log("reqModel", reqModel);
     db[reqModel].insert(reqData.item, function (err, data) {
         if (data) {
             db[reqModel].find({}, function (err, data) {
@@ -64,15 +109,13 @@ export const addAnItem = (req, res) => {
 }
 
 export const removeAnItem = (req, res) => {
-    let uid = req.body.uid;
-    let reqData = req.body;
+    let uid = req.body.options.uid;
+    let reqData = req.body.options;
     if (!reqData || !reqData.model) {
         return res.json({ 'success': false, 'message': 'Model Error' });
     }
-    let reqModel = reqData.model || 'widgets'
+    let reqModel = reqData.model //|| 'widgets'
     db[reqModel].remove({ uid: reqData.uid }, function (err, data) {
-        // console.log('err=', err);
-        // console.log('data=', data);
         if (data) {
             db[reqModel].find({}, function (err, data) {
                 return res.json(data);
